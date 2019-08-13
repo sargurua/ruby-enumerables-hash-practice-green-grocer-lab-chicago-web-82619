0a3e340838c2ce83aa11bad43fb44803b5f8a1fd
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-13 00:18:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def apply_coupons(cart, coupons)
     return cart
   else
     coupons.each do |i|
-      if cart.include?(coupons[:item]) && 
+      if cart.include?(coupons[:item]) && cart[]
       
     end
   end
