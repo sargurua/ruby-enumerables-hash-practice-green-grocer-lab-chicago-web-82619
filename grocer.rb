7ac9aba8a629611d19f1461fca9fbc0d719e600b
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-13 02:50:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,21 +21,25 @@ def apply_coupons(cart, coupons)
     return cart
   else
     new_cart = cart
-    coupons.each do |i|
-      item_name = i[:item]
-      item_count = i[:num]
-      if cart.include?(item_name) && cart[item_name][:count] >= item_count
-        new_cart[item_name][:count] = -item_count
-        new_cart["#{item_name} W/COUPON"] ={
-          :price => i[item_name][:cost] / new_cart[item_name][:count],
-          :clearance => true,
-          :count => i[item_name][:count]
-        }
-      end
-    end
-  end
-  return new_cart
+    coupons.each do |coupon|
+      name = coupon[:item] #avocado, cheese,...
+      num_of_c = coupon[:num]
+      if cart.include?(name) && cart[name][:count] >= num_of_c
+        new_cart[name][:count] -= num_of_c
+        if new_cart["#{name} W/COUPON"]
+          new_cart["#{name} W/COUPON"][:count] += 1
+        else
+          new_cart["#{name} W/COUPON"] = {
+            :price => coupon[:cost],
+            :clearance => new_cart[name][:clearance],
+            :count => 1
+          }
+       end
+     end
+   end
+   new_cart
 end
+
 
 def apply_clearance(cart)
   # code here
